b441b503dd51619f4d6fc00d038d29373d0c0379
Jeremy Chone <jeremy.chone@gmail.com>
2018-06-05 18:30:11 -0700

    . fix compile issue (views/ path was wrong)

--- web/src/ts/main.ts
+++ web/src/ts/main.ts
@@ -1,6 +1,6 @@
 import { on, display, first } from "mvdom";
 import { route } from "./route";
-import { MainView } from "view/MainView";
+import { MainView } from "views/MainView";
 import { get } from "ts/ajax";
 
 
